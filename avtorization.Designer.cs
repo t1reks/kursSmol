6903e6cd3f90a983d23280d7f6a09b4848b29890
kursSmol <smolik-2005@mail.ru>
2024-06-25 08:41:08 +0300

    add avtorization.Designer.cs

---
+++ avtorization.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace kursSmol
+{
+    partial class avtorization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // avtorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "avtorization";
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.avtorization_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
